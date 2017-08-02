13111a65e55b97676c9a5467a5233df1d208f17e
Arron Hartley <arronhartley@gmail.com>
2017-08-02 08:18:23 +0100

    Initial commit with basic ideas

---
+++ FSUIPCInterface.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using FSUIPC;
+
+namespace VAP3D
+{
+    public class FSUIPCInterface
+    {
+        private static int CONNECT_SLEEP_TIME = 1000;
+        private static int EVENT_LOOP_DELAY = 10000;
+
+        private dynamic m_vaProxy;
+
+        private bool m_isConnected;
+
+        private Thread m_connectionThread;
+        private Thread m_eventMonitorThread;
+
+        // -----------------------------------------
+        // Private methods
+        private bool tryConnect()
+        {
+            try
+            {
+                FSUIPCConnection.Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        private void connectionRunner()
+        {
+            try
+            {
+                while (!m_isConnected)
+                {
+                    if (tryConnect())
+                    {
+                        m_isConnected = true;
+                        m_vaProxy.WriteToLog("Connected to FSUIPC", "green");
+                    }
+                    else
+                    {
+                        Thread.Sleep(CONNECT_SLEEP_TIME);
+                    }
+                }
+            }
+            catch(ThreadAbortException e)
+            {
+            }
+        }
+
+        private void process()
+        {
+            FSUIPCConnection.Process();
+        }
+
+        private void writeErrorToLog(string error)
+        {
+            m_vaProxy.WriteToLog("VA:P3D Error: " + error, "red");
+        }
+
+        private void eventMonitorRunner()
+        {
+
+        }
+
+        // Public Interface
+        public void beginMonitoringEvents()
+        {
+            if (!m_isConnected)
+            {
+                writeErrorToLog("Not connected");
+                return;
+            }
+
+            m_eventMonitorThread = new Thread(eventMonitorRunner);
+            m_eventMonitorThread.Start();
+        }
+
+        public void stopMonitoringEvents()
+        {
+            if (!m_isConnected)
+            {
+                writeErrorToLog("Not connected");
+                return;
+            }
+
+            m_eventMonitorThread.Abort();
+            m_eventMonitorThread.Join();
+
+            m_eventMonitorThread = null;
+        }
+
+        public void readOffset(int offsetAddress, int numBytes, string destinationVariable)
+        {
+        }
+
+        public void writeOffset(int offsetAddress, int numBytes, string sourceVariable)
+        {
+
+        }
+
+        public void initialise(dynamic vaProxy)
+        {
+            m_vaProxy = vaProxy;
+            m_connectionThread = new Thread(() => connectionRunner());
+            m_connectionThread.Start();
+        }
+
+        public void shutdown()
+        {
+            if (m_connectionThread.IsAlive)
+            {
+                m_connectionThread.Abort();
+            }
+            if (m_eventMonitorThread.IsAlive)
+            {
+                m_eventMonitorThread.Abort();
+            }
+            if (m_isConnected)
+            {
+                FSUIPCConnection.Close();
+            }
+        }
+
+        public FSUIPCInterface()
+        {
+            m_isConnected = false;
+            m_vaProxy = null;
+        }
+    }
+}
---
+++ VoiceAttackPlugin.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace VAP3D
+{
+    public class VoiceAttackPlugin
+    {
+        public static string VA_DisplayName()
+        {
+            return "VA:P3D Plugin";
+        }
+
+        public static string VA_DisplayInfo()
+        {
+            return "My VoiceAttack Plugin\r\n\r\nThis is just a sample.\r\n\r\n2016 VoiceAttack";
+        }
+
+        public static Guid VA_Id()
+        {
+            return new Guid("{20a8e798-1848-4878-92e3-865f4ec48063}");
+        }
+
+        public static void VA_StopCommand()
+        {
+        }
+
+        public static void VA_Init1(dynamic vaProxy)
+        {
+            FSUIPCInterface fsuipcInterface = new FSUIPCInterface();
+            fsuipcInterface.initialise(vaProxy);
+
+            vaProxy.SessionState.Add("FSUIPCInterface", fsuipcInterface);
+        }
+
+        public static void VA_Exit1(dynamic vaProxy)
+        {
+            FSUIPCInterface fsuipcInterface = vaProxy.SessionState["FSUIPCInterface"];
+            fsuipcInterface.shutdown();
+        }
+
+        public static void VA_Invoke1(dynamic vaProxy)
+        {
+            FSUIPCInterface fsuipcInterface = vaProxy.SessionState["FSUIPCInterface"];
+
+            string context = vaProxy.Context;
+
+            ParsedFunction func = parseFunction(context);
+            fsuipcInterface.call(func.Function, func.ArgList);
+        }
+    }
+}
